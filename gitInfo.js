7d767aa1deda7492da01b3bd777620b25a0130e1
dithiane <elenamena.colorado@gmail.com>
2023-04-08 15:18:08 -0600

    Initial commit

---
+++ gitInfo.js
@@ -0,0 +1,108 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+//CODE HERE
+console.log("Problem1-------------------------------------------------------------")
+
+let gitDefinition = "Git is a console utility for tracking and maintaining a history of file changes in your project."
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+//CODE HERE
+
+console.log("Problem2-------------------------------------------------------------")
+
+let gitHubDefinition = "GitHub is a web service based on the Git system, which helps to conveniently carry out the collective development of IT projects: publish, edit code, comment on other people's developments."
+
+//////////////////PROBLEM 3////////////////////
+/*
+    Create a variable called 'gitInitDefinition'.  
+    It should be a string containing your best definition of what 'git init' does.
+*/
+
+//CODE HERE
+
+console.log("Problem3-------------------------------------------------------------")
+
+let gitInitDefinition = "'git init' creates a new Git repository locally, also it can be used to convert an existing untracked project to a Git repository, or to initialize a new empty repository."
+
+//////////////////PROBLEM 4////////////////////
+/*
+    Create a variable called 'gitCloneDefinition'.  
+    It should be a string containing your best definition of what 'git clone' does.
+*/
+
+//CODE HERE
+
+console.log("Problem4-------------------------------------------------------------")
+
+let gitCloneDefinition = " 'git clone' is command copies an existing Git repository and this 'working copy' is a full-fledged Git repository with its own history and files, completely separate from the original repository."
+
+//////////////////PROBLEM 5////////////////////
+
+/*
+    Create a variable called 'gitStatusDefinition'.  
+    It should be a string containing your best definition of what 'git status' does.
+*/
+
+//CODE HERE
+
+console.log("Problem5-------------------------------------------------------------")
+
+let gitStatusDefinition = " 'git status' is command displays the status of the working directory, checks the changes and see files that are not tracked by Git."
+
+//////////////////PROBLEM 6////////////////////
+/*
+    Create a variable called 'gitAddDefinition'.  
+    It should be a string containing your best definition of what 'git add' does.
+
+    Create another variable called 'gitAddCode'.  
+    It should be a string containing the code to add all files.
+*/
+
+//CODE HERE
+
+console.log("Problem6-------------------------------------------------------------")
+
+let gitAddDefinition = " 'git add' moves pending changes from the 'working directory' to the indexed files section."
+let gitAddCode = "git add ."
+
+//////////////////PROBLEM 7////////////////////
+/*
+    Create a variable called 'gitCommitDefinition'.  
+    It should be a string containing your best definition of what 'git commit' does.
+
+    Create a variable called 'gitCommitCode'.  
+    It should be a string containing the code to commit using the message "initial commit".
+*/
+
+//CODE HERE
+
+console.log("Problem7-------------------------------------------------------------")
+
+let gitCommitDefinition = "'git commit' takes a snapshot of the current state of the changes added to the indexed files section."
+let gitCommitCode = "git commit -m 'initial commit'"
+
+//////////////////PROBLEM 8////////////////////
+/*
+    Create a variable called 'gitPushDefinition'.  
+    It should be a string containing your best definition of what 'git push' does.
+*/
+
+//CODE HERE
+
+console.log("Problem7-------------------------------------------------------------")
+
+let gitPushDefinition= "'git push' allows to push commits from a local repository to a remote one."
---
+++ sdlc.js
@@ -0,0 +1,26 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisRequirements = "This is the second step, defined necessary timeline to finish the work and get detailed and precise requirements for the system."
+const design = "This is the third step, in which the system and software design documents are prepared as per the requirement specification document, that helps define overall system architecture."
+const implementation = "This is the forth step, where tasks are divided into units or modules and assigned to the various developers, and they start build this units."
+const testingIntegration = "This is the fifth step, in which a testing team starts testing the functionality of the entire system and verify that the entire application works according to the customer requirement."
+const maintain = "This is the sixth step, follows after the system is deployed, that provides the bug fixing, upgrade the application or adding some new fixtures."
